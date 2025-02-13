8eb40bc1497271b4418cecdec94e3e8a244b04dd
Alexsandro Oliveira <127613673+Alexsandro-Oliveira-Jr@users.noreply.github.com>
2025-02-13 23:27:40 +0000

    introdução a poo.

---
+++ poo/Main.java
@@ -0,0 +1,15 @@
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("marta", 31, "Email@.com");
+        
+       System.out.println("Nome: " + pessoa.getNome());
+       System.out.println("idade: " + pessoa.getIdade());
+       System.out.println("Email: " + pessoa.getEmail());
+    
+ 
+    } 
+ }
